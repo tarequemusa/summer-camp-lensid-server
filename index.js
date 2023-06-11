2ff40967a482849e6d9540bf70a571d605e4d02d
tarequemusa <tareque.web@gmail.com>
2023-06-12 01:38:09 +0600

    API Created for admin verify with JWT

--- index.js
+++ index.js
@@ -17,6 +17,23 @@ app.use(cors(corsOptions))
 app.use(express.json());
 
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization) {
+        return res.status(401).send({error: true, message: 'unauthorized access'});
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err) {
+            return res.status(401).send({error: true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
 
 const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@cluster0.g5abh6e.mongodb.net/?retryWrites=true&w=majority`;
@@ -41,12 +58,27 @@ async function run () {
         const cartCollection = client.db("lensIdDb").collection("carts");
 
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+
+            res.send({token})
+        })
+
+
         // cart collection apis
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
+
             if(!email) {
                 return res.send([]);
             }
+
+            const decodedEmail = req.decoded.email;
+            if(email !== decodedEmail) {
+                return res.status(403).send({error: true, message: 'Forbidden access'})
+            }
+
             const query = {email: email};
             const result = await cartCollection.find(query).toArray();
             res.send(result);
@@ -68,14 +100,14 @@ async function run () {
             console.log(result);
         })
 
-        // Class Collection:
+        // Class Collection APIs:
 
         app.get('/class', async (req, res) => {
             const result = await classCollection.find().sort({seats_available: -1}).toArray();
             res.send(result);
         })
 
-        // instructor Collection:
+        // instructor Collection APIs:
 
         app.get('/instructor', async (req, res) => {
             const result = await instructorCollection.find().sort({enrolled_students: -1}).toArray();
@@ -101,6 +133,21 @@ async function run () {
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
+
+
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if(req.decoded.email !== email) {
+                res.send({admin: false})
+            }
+
+            const query = {email: email}
+            const user = await usersCollection.findOne(query);
+            const result = {admin: user?.role === 'admin'}
+            res.send(result);
+        })
+
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;

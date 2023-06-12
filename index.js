cf78c685114b476be333e255ba02cb108b9290a2
tarequemusa <tareque.web@gmail.com>
2023-06-12 17:10:53 +0600

    Instructor Dashboard API created

--- index.js
+++ index.js
@@ -148,6 +148,19 @@ async function run () {
             res.send(result);
         })
 
+        app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if(req.decoded.email !== email) {
+                res.send({instructor: false})
+            }
+
+            const query = {email: email}
+            const user = await usersCollection.findOne(query);
+            const result = {instructor: user?.role === 'instructor'}
+            res.send(result);
+        })
+
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
